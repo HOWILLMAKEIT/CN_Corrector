9b076682c95d5751677400ca0a79d4268d005ffd
HOWILL <1450656923@qq.com>
2025-03-13 12:21:28 +0800

    CNCorrector_v1

---
+++ CNCorrector.py
@@ -0,0 +1,47 @@
+import sys
+
+sys.path.append("..")
+from pycorrector.macbert.macbert_corrector import MacBertCorrector
+
+import spacy
+
+nlp = spacy.load("zh_core_web_sm")
+
+def split_sentences(text):
+    """将大段中文文本分割为句子列表"""
+    # 预处理文本，移除换行符和制表符
+    text = text.replace('\n', '').replace('\t', '')
+    doc = nlp(text)
+    # 获取句子并删除每个句子中的空格和双引号
+    return [sent.text.strip().replace(' ', '').replace('“', '').replace('”', '').lower() for sent in doc.sents]
+ 
+long_text = """
+能花时间再这个项目上的也是神人了
+"""
+sentences = split_sentences(long_text)
+
+
+if __name__ == '__main__':
+    error_sentences = sentences
+
+    m = MacBertCorrector("shibing624/macbert4csc-base-chinese")
+    for i, sent in enumerate(sentences):
+        print(f"{i+1}. {sent}")
+    
+    print("\n开始纠错:")
+    for line in sentences:
+        try:
+            # 确保输入不为空
+            if not line.strip():
+                continue
+            # 进行纠错
+            correct_sent, err = m.macbert_correct(line)
+            if err!= []:
+                # 打印结果
+                print('-----------------------------------------********************------------------------')
+                print("原文: {}".format(line))
+                print("纠正: {}".format(correct_sent))
+                print("错误: {}".format(err))
+        except Exception as e:
+            print(f"处理句子时出错: {line}")
+            print(f"错误信息: {e}")
---
+++ Readme.md
@@ -0,0 +1,57 @@
+# introduction
+
+本项目借助开源工具[pycorrector](https://github.com/shibing624/pycorrector)和开源模型[spaCy](https://github.com/explosion/spaCy)实现了一个长文本的自动分句+纠错，其中分句采用spaCy中文模型zh_core_web_sm处理，而纠错则采用pycorrector提供的预训练好的MacBERT4CSC模型。
+
+该项目能够进行的中文文本纠错类型如下（图片出自pycorrector)：
+
+​	![error_type.png](Readme.assets/error_type-1741838606716-3.png)
+
+# Usage
+
+首先确保你已经安装了[docker](https://www.docker.com/)
+
+随后打开命令行运行
+
+```
+docker run 您的用户名/app-name:标签
+```
+
+打开vscode，安装Dev Containers插件，随后按F1，输入：
+
+```
+Dev containers: Attach to Running container
+```
+
+选择正在运行的容器，随后就可以在左侧看到cncorrector.py文件，文件中包含了一个我的测试案例（我的习概论文），可以直接运行检查是否缺少环境。
+
+在cncorrector.py文件中，将long_text字符串的值替换为你的待检测文本即可运行。
+
+如果你的文本已经做好了分句，也可以直接替换error_sentences字符串的值，注意格式类似：
+
+```python
+error_sentences = [
+        '真麻烦你了。希望你们好好的跳无',
+        '少先队员因该为老人让坐',
+    ]
+```
+
+# 使用示例
+
+本项目的优势在于开盖即用，适合懒得折腾的朋友。
+
+对于专业性较高的文本，例如计算机网络课本，纠错效果一般
+
+适用于平时的小论文/演讲稿的纠错
+
+例如我将我大一写的习概论文（大约3000字）直接喂给模型，得到的输出如下：
+
+![image-20250313121644413](Readme.assets/image-20250313121644413.png)
+
+可以看到其将文本分为73个句子并检测出了4个错误。
+
+第一个错误是打字的时候多打了一个“哦”。
+
+第二个错误是“用时政府”，我也不知道为什么我会写这几个字（可能也是打快了）。
+
+第三个错误是我将“规模”打成了“估模”，虽然模型未能正确纠错，但是成功地发现了错误。
+
